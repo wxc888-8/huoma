efdcf98a954a82bcf8aa4ef30e1075f3763c27f3
wxc888-8 <wxc888-8@example.com>
2026-04-13 15:29:46 +0800

    Add all ignored files

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+/*数据库配置*/
+$dbconfig=array(
+	'host' => 'localhost', //数据库服务器
+	'port' => 3306, //数据库端口
+	'user' => 'huomaxit', //数据库用户名
+	'pwd' => 'LHatxNPBrzjBeRtD', //数据库密码
+	'dbname' => 'huomaxit', //数据库名
+	'dbqz' => 'dwz' //数据表前缀
+);
+?>
